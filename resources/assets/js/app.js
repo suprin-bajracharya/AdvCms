88b0f2c9544a47c1c6533c2f6bb2dec712eb59b8
suprin-bajracharya <bajrasuprin@gmail.com>
2017-06-24 17:29:04 +0545

    Site Configuration of CSS and JS

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,15 +8,10 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Buefy from 'buefy';
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.use(Buefy);
 
-Vue.component('example', require('./components/Example.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+//Vue.component('example', require('./components/Example.vue'));
+
